6e39ac99e02ebd3d7735dfb0348fadef2261e354
1984secant <127aacrooms@frhsd.com>
2025-12-16 14:04:06 -0500

    hlelo

--- script.js
+++ script.js
@@ -12,9 +12,17 @@ let ad2 = document.getElementById("adj");
 let sbm = document.getElementById("submit");
 let words = document.querySelectorAll("input[type='text']");
 
+let placeholder = document.querySelectorAll(".userInput");
+
+
+console.log(words);
 sbm.addEventListener('click', madlib)
 
 function madlib() {
-    document.getElementById("mads").innerHTML = " " + who.value + " enters a " + whereType.value + " that\’s called" + where.value + ". Inside that place, they go and" + verb.value + " a " + ad.value + fd.value + ". While approaching the register, their friend "+ who.value +" enters the building. "+ who2.value +" approaches "+ who.value +" and asks for their" + what.value +", only to get "+ vd.value +" by "+ who.value + ". The cashier takes a look at the scene and pays it no mind, waiting for "+ who.value +" to finally get to the register. The cashier\’s nametag reads "+ ad2.value +" rather than a usual name, which makes "+ who.value + "raise an eyebrow. Finally the food that was originally picked up was " + who.value + ", and "+ who.value + " leaves the building. Typical";
+    placeholder.forEach((placeholder, index) => {
+    console.log(placeholder, index);
+    console.log(words[index.value]);
+    })
+    // document.getElementById("mads").innerHTML = " " + who.value + " enters a " + whereType.value + " that\’s called" + where.value + ". Inside that place, they go and" + verb.value + " a " + ad.value + fd.value + ". While approaching the register, their friend "+ who.value +" enters the building. "+ who2.value +" approaches "+ who.value +" and asks for their" + what.value +", only to get "+ vd.value +" by "+ who.value + ". The cashier takes a look at the scene and pays it no mind, waiting for "+ who.value +" to finally get to the register. The cashier\’s nametag reads "+ ad2.value +" rather than a usual name, which makes "+ who.value + "raise an eyebrow. Finally the food that was originally picked up was " + who.value + ", and "+ who.value + " leaves the building. Typical";
 console.log(document.getElementById("mads").innerHTML);
 }
